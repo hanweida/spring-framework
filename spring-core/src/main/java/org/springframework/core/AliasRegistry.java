612a992c1efc6f634f75dda34f12e0d20ff2cc08
yingmule <534446340@qq.com>
2020-12-07 18:06:30 +0800

    Commit Final

--- spring-core/src/main/java/org/springframework/core/AliasRegistry.java
+++ spring-core/src/main/java/org/springframework/core/AliasRegistry.java
@@ -19,7 +19,7 @@ package org.springframework.core;
 /**
  * Common interface for managing aliases. Serves as super-interface for
  * {@link org.springframework.beans.factory.support.BeanDefinitionRegistry}.
- *
+ * 测试
  * @author Juergen Hoeller
  * @since 2.5.2
  */
